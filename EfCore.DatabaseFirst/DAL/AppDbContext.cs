6a4ecfeb9d31b35fed9f7cc286570335004e788a
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-05 22:37:31 +0300

    added database first

---
+++ EfCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.DatabaseFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = .\\sqlexpress; Initial Catalog = EfCoreDatabaseFirstDb; Integrated Security = True; TrustServerCertificate = True;");
+        }
+
+    }
+}
--- EfCore.DatabaseFirst/Program.cs
+++ EfCore.DatabaseFirst/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using EfCore.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    });
+    
+}
