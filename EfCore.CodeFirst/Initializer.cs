57ae25c67e31881413f8ccd92ca13bc2e99e2d7e
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-06 14:45:16 +0300

    added code first project

---
+++ EfCore.CodeFirst/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.CodeFirst
+{
+    public class Initializer
+    {
+        public static IConfigurationRoot Configuration;
+
+        public static void Build()
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = configuration.Build();
+        }
+    }
+}
---
+++ EfCore.CodeFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EfCore.CodeFirst;
+using EfCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+Initializer.Build();
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name} - {p.Price} - {p.Stock}");
+    });
+
+}
