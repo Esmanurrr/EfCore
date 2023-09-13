3411cd27270a29b31857fb4569017d870c6e3090
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-13 10:32:43 +0300

    explicit loading

--- EfCore.CodeFirst/Program.cs
+++ EfCore.CodeFirst/Program.cs
@@ -8,20 +8,29 @@ Console.WriteLine("Hello, World!");
 Initializer.Build();
 using (var _context = new AppDbContext())
 {
-    //var category = new Category() { Name = "Kalemler" };
-    //category.Products.Add(new Product() { Name = "Kalem 1", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new() { Color = "Red", Height = 100, Width = 200 } });
-    //category.Products.Add(new Product() { Name = "Kalem 2", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new() { Color = "Blue", Height = 100, Width = 200 } });
+    //category üzerinden getirme
+    //var category = _context.Categories.First();
+    ////
+    ////
+    ////
+    //if (true)
+    //{
+    //    _context.Entry(category).Collection(x => x.Products).Load();
+    //    category.Products.ForEach(product =>
+    //    {
+    //        Console.WriteLine(product.Name);
+    //    });
+    //};
 
-    //_context.Add(category);
-    //category üzerinden ekleme
-    //var categoryWithProducts = _context.Categories.Include(c => c.Products).ThenInclude(p => p.ProductFeature).First();
-
-    //product feature üzerinden ekleme
-    //var productFeature = _context.ProductFeatures.Include(x => x.Product).ThenInclude(c=>c.Category).First();
-
-    //product üzerinden ekleme
-    //var product = _context.Products.Include(x => x.ProductFeature).Include(y => y.Category).First(); --> iki tane navigation propertysi var o yüzden then kullanmadık
-
+    var product = _context.Products.First();
+    //
+    //
+    //
+    if (true)
+    {
+        _context.Entry(product).Reference(x => x.ProductFeature).Load();
+        
+    };
 
     Console.WriteLine("İşlem bitti.");
 
